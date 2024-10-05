7397ad055566b55339a82fe04ab959789490b63d
nino.alamo <nin.alamo@re-leased.com>
2024-10-05 10:13:38 +0800

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,72 @@
+﻿namespace ClassLibrary1
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+        public string AccountName { get; set; }
+
+        public BankAccount(string accountName, decimal initialBalance)
+        {
+            AccountName = accountName;
+            _balance = initialBalance;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            _balance -= amount;
+        }
+    }
+
+    public class Customer
+    {
+        private string _name;
+        private int _age;
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+
+        public Customer(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string GetCustomerDetails()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+
+    public class Employee
+    {
+        protected string _employeeId;
+        public Employee(string employeeId)
+        {
+            _employeeId = employeeId;
+        }
+
+        public string GetEmployeeId()
+        {
+            return _employeeId;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string Department { get; set; }
+
+        public Manager(string employeeId, string department) : base(employeeId)
+        {
+            Department = department;
+        }
+    }
+}
---
+++ TestProject1/AbstractAndInterfacesTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class AbstractAndInterfacesTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AbstractShape_Should_Throw_Exception_When_Instantiated()
+        {
+            // Act & Assert
+            Shape shape = new Shape();  // Should throw exception as Shape is abstract
+        }
+
+        [TestMethod]
+        public void Circle_Should_Override_AbstractMethods_Correctly()
+        {
+            // Arrange
+            Circle circle = new Circle { Radius = 3 };
+
+            // Act
+            var area = circle.Area();
+
+            // Assert
+            Assert.AreEqual(28.2743, area, 0.0001);
+        }
+
+        [TestMethod]
+        public void Rectangle_Should_Override_AbstractMethods_Correctly()
+        {
+            // Arrange
+            Rectangle rectangle = new Rectangle { Width = 5, Height = 2 };
+
+            // Act
+            var area = rectangle.Area();
+
+            // Assert
+            Assert.AreEqual(10, area);
+        }
+    }
+}
+
